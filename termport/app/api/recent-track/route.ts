c8381ef0e2580fa34c58e8ef5d1f908c0b40a0b5
sreehari <sreehari9300@gmail.com>
2025-03-08 22:52:58 +0530

    fixed vercel caching problem

--- termport/app/api/recent-track/route.ts
+++ termport/app/api/recent-track/route.ts
@@ -2,11 +2,19 @@ import { NextResponse } from "next/server";
 
 export async function GET(){
     try{
-        const res = await fetch("https://spotiserver-yvnu.onrender.com/recent-track");
+        const res = await fetch("https://spotiserver-yvnu.onrender.com/recent-track",{
+            headers:{
+                'Cache-control':'no-store'
+            }
+        });
         if(!res.ok) throw new Error("failed to fetch data");
 
         const data = await res.json();
-        return NextResponse.json(data);
+        return NextResponse.json(data,{
+            headers:{
+                'Cache-control':'no-store,no-cache,must-revalidate,proxy-revalidate'
+            }
+        });
     }catch(error){
         return NextResponse.json({
             error:"failed to fetch the last played track",
